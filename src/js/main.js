4abc3316f1cb034f05cb674e5f9cb679560b3ebc
Marat Sarvarov <shtirlizc@gmail.com>
2021-03-25 10:53:58 +0500

    modals

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,18 @@ import { map } from "./map";
 import { mobileMenu } from "./mobileMenu";
 import { scroll } from "./scroll";
 import { table } from "./table";
+import { textField } from "./textField";
+import { phoneMask } from "./phoneMask";
+import { Modal } from "./modal";
+
+window.Modal = Modal;
 
 window.addEventListener("load", () => {
   mobileMenu();
   scroll();
   table();
   map();
+  textField();
+  phoneMask();
+  Modal.init();
 });
